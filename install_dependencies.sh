5880e987fa39e9e1214765aad62b2e18614f3e25
jonahedmundson <jonahedmundson>
2022-12-18 19:04:37 -0800

    First test done!

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,7 +3,7 @@
 sudo apt install firefox
 conda install selenium
 conda install pandas
-pip install geckodriver
+pip install webdrivermanager #geckodriver
 conda install urllib3
 pip install googletrans==3.1.0a0
 pip install IPython
